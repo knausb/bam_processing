901775ffb3d8238e86824713500d3d964e86396b
Brian Knaus <briank.lists@gmail.com>
2020-03-27 22:14:32 -0700

    worked on read_alignment_2020_tmp_pe.sh and genotype_gVCF_2020.sh

---
+++ genotype_gVCF_2020.sh
@@ -0,0 +1,64 @@
+#! /bin/bash
+
+#$ -N gtgvcf
+#$ -o gtgvcfout
+#$ -e gtgvcferr
+#$ -V
+#$ -cwd
+#$ -S /bin/bash
+#$ -q !gbs
+
+# #$ -l mem_free=2G
+# $ -p -10
+# #$ -h
+# $ -t 50-50:1
+# $ -t 5-4921
+
+i=$(expr $SGE_TASK_ID - 1)
+
+GATK="~/bin/gatk4/gatk-4.1.4.1/gatk"
+
+JAVA="/home/bpp/knausb/bin/javadir/jre1.8.0_25/bin/java"
+
+
+REF="/home/bpp/knausb/Grunwald_Lab/home/knausb/bjk_pinf_ref/pinf_super_contigs.fa"
+
+#FILE=( `cat "bams1.txt" `)
+
+#IFS=',' read -r -a arr <<< "${FILE[$i]}"
+
+echo -n "Running on: "
+hostname
+echo "SGE job id: $JOB_ID"
+date
+echo
+
+CMD="$JAVA -version 2>&1"
+echo $CMD
+eval $CMD
+echo
+
+CMD="$JAVA -jar $GATK --version"
+echo $CMD
+eval $CMD
+echo
+
+# https://www.broadinstitute.org/gatk/documentation/article?id=3893
+# https://www.broadinstitute.org/gatk/documentation/tooldocs/org_broadinstitute_gatk_engine_CommandLineGATK.php
+# https://www.broadinstitute.org/gatk/documentation/tooldocs/org_broadinstitute_gatk_tools_walkers_haplotypecaller_HaplotypeCaller.php
+# https://www.broadinstitute.org/gatk/documentation/tooldocs/org_broadinstitute_gatk_tools_walkers_variantutils_GenotypeGVCFs.php
+
+CMD="$JAVA -Djava.io.tmpdir=/data/ \
+  -jar $GATK \
+  -T GenotypeGVCFs \
+  -R $REF \
+  -L Supercontig_1.$SGE_TASK_ID \
+  -V gvcfs.list \
+  -o vcfs/sc_1.$SGE_TASK_ID.vcf.gz"
+
+echo $CMD
+eval $CMD
+
+date
+
+# EOF.
